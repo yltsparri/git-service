64fd50f8a7938a358185bdbcdd9ef81b336a2001
Ülo Parri <yltsparri@gmail.com>
2017-11-20 21:27:05 +0200

    Use Java 9

--- src/main/java/com/example/ValidationExceptionMapper.java
+++ src/main/java/com/example/ValidationExceptionMapper.java
@@ -2,6 +2,7 @@ package com.example;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
@@ -9,8 +10,9 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class ValidationExceptionMapper implements ExceptionMapper<javax.validation.ValidationException> {
+
     @Override
-    public Response toResponse(javax.validation.ValidationException e) {
+    public Response toResponse(ValidationException e) {
         final StringBuilder strBuilder = new StringBuilder();
         for (ConstraintViolation<?> cv : ((ConstraintViolationException) e).getConstraintViolations()) {
             strBuilder.append(cv.getMessage());
